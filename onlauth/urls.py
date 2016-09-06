09dd8458856b7f9215f1ad0fa411f0a4335d43b8
aakash-cr7 <aakash.tyg93@gmail.com>
2016-09-06 20:51:16 +0530

    Add github,linkedin urls.

--- onlauth/urls.py
+++ onlauth/urls.py
@@ -2,9 +2,8 @@ from django.conf.urls import include, url
 from django.contrib import admin
 
 urlpatterns = [
-    # Examples:
-    # url(r'^$', 'onlauth.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^linkedin/$', 'onlauth.views.linkedin_connect', name='linkedin_connect'),
+    url(r'^github/$', 'onlauth.views.github_connect', name='github_connect'),
 ]
